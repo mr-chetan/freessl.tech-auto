ab1c0d447ea7a79df0e955098f9a816dc398adce
Anindya Sundar Mandal <support@speedify.tech>
2019-01-18 16:58:15 +0530

    Update Base64UrlSafeEncoder.php

--- FreeSSLAuto/src/Acme/Base64UrlSafeEncoder.php
+++ FreeSSLAuto/src/Acme/Base64UrlSafeEncoder.php
@@ -1,15 +1,30 @@
 <?php
 
 /**
- * PHP version 5.4
  *
- * @author     Anindya Sundar Mandal <anindya@SpeedUpWebsite.info>
- * @copyright  2018 Anindya Sundar Mandal; Copyright (c) 2015, Stanislav Humplik <sh@analogic.cz>
- * @license    https://opensource.org/licenses/BSD-3-Clause  BSD 3-Clause "New" or "Revised" License
- * @version    1.0.0
+ * @package FreeSSL.tech Auto
+ * This PHP app issues and installs free SSL certificates in cPanel shared hosting with complete automation.
+ * 
+ * @author Anindya Sundar Mandal <anindya@SpeedUpWebsite.info>
+ * @copyright  Copyright (C) 2018-2019, Anindya Sundar Mandal
+ * @license    http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License, version 3
  * @link       https://SpeedUpWebsite.info
  * @since      Class available since Release 1.0.0
+ * 
  *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
  */
 
 namespace FreeSslDotTech\FreeSSLAuto\Acme;
