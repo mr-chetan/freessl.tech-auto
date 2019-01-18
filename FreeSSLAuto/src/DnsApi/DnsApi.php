c7b8a1690e274fef9bf6f52b1633051203a4ad5b
Anindya Sundar Mandal <support@speedify.tech>
2019-01-18 17:05:49 +0530

    Update DnsApi.php

--- FreeSSLAuto/src/DnsApi/DnsApi.php
+++ FreeSSLAuto/src/DnsApi/DnsApi.php
@@ -1,16 +1,30 @@
 <?php
 
 /**
- * PHP version 5.4.
  *
- * @author     Anindya Sundar Mandal <anindya@SpeedUpWebsite.info>
- * @copyright  2018 Anindya Sundar Mandal
- * @license    https://opensource.org/licenses/BSD-3-Clause  BSD 3-Clause "New" or "Revised" License
- *
- * @version    1.0.0
- *
- * @see       https://SpeedUpWebsite.info
+ * @package FreeSSL.tech Auto
+ * This PHP app issues and installs free SSL certificates in cPanel shared hosting with complete automation.
+ * 
+ * @author Anindya Sundar Mandal <anindya@SpeedUpWebsite.info>
+ * @copyright  Copyright (C) 2018-2019, Anindya Sundar Mandal
+ * @license    http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License, version 3
+ * @link       https://SpeedUpWebsite.info
  * @since      Class available since Release 1.0.0
+ * 
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
  */
 
 namespace FreeSslDotTech\FreeSSLAuto\DnsApi;
@@ -135,7 +149,7 @@ class DnsApi
         $body .= "Do not reply to this automated email.<br /><br />
         --------------<br />
         FreeSSL.tech Auto<br />
-        Powered by <a href='https://letsencrypt.org'>Let’s Encrypt</a>, <a href='https://speedify.tech'>SpeedUpWebsite.info</a> and <a href='https://getwww.me'>GetWWW.me</a><br /><br />
+        Powered by <a href='https://letsencrypt.org'>Letâ€™s Encrypt</a>, <a href='https://speedify.tech'>SpeedUpWebsite.info</a> and <a href='https://getwww.me'>GetWWW.me</a><br /><br />
         </body></html>";
 
         //Send email to all admin email id
